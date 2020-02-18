2cce1bc5ae716fb52b835caac03fd09d44c32d24
mdb293 <mdb293@cornell.edu>
2020-02-18 11:15:05 -0500

    made scrambled condition include only continuous objects and not symbols that are imagined. Swapped + for H

--- Scaffolded_Attn_Pilot/expt_scaffvivid_sd.py
+++ Scaffolded_Attn_Pilot/expt_scaffvivid_sd.py
@@ -1,3 +1,7 @@
+# Use older version of psychopy
+import psychopy
+psychopy.useVersion('1.90')
+
 # from psychopy import gui
 from psychopy import visual, core, event, data
 import csv
@@ -30,31 +34,39 @@ class Runtrial:
         self.fontSize = 0.5
         self.fontColor = "black"
 
-        self.conditions = [["original", "H", "change", "easyDark"], ["original", "L", "change", "easyDark"],
-                           ["original", "T", "change", "easyDark"], ["scrambled", "H", "change", "easyDark"],
-                           ["scrambled", "L", "change", "easyDark"], ["scrambled", "T", "change", "easyDark"],
-                           ["original", "H", "change", "hardDark"], ["original", "L", "change", "hardDark"],
-                           ["original", "T", "change", "hardDark"], ["scrambled", "H", "change", "hardDark"],
-                           ["scrambled", "L", "change", "hardDark"], ["scrambled", "T", "change", "hardDark"],
-                           ["original", "H", "change", "easyBright"], ["original", "L", "change", "easyBright"],
-                           ["original", "T", "change", "easyBright"], ["scrambled", "H", "change", "easyBright"],
-                           ["scrambled", "L", "change", "easyBright"], ["scrambled", "T", "change", "easyBright"],
-                           ["original", "H", "change", "hardBright"], ["original", "L", "change", "hardBright"],
-                           ["original", "T", "change", "hardBright"], ["scrambled", "H", "change", "hardBright"],
-                           ["scrambled", "L", "change", "hardBright"], ["scrambled", "T", "change", "hardBright"],
-                           ["original", "H", "no change", "easyDark"], ["original", "L", "no change", "easyDark"],
-                           ["original", "T", "no change", "easyDark"], ["scrambled", "H", "no change", "easyDark"],
-                           ["scrambled", "L", "no change", "easyDark"], ["scrambled", "T", "no change", "easyDark"],
-                           ["original", "H", "no change", "hardDark"], ["original", "L", "no change", "hardDark"],
-                           ["original", "T", "no change", "hardDark"], ["scrambled", "H", "no change", "hardDark"],
-                           ["scrambled", "L", "no change", "hardDark"], ["scrambled", "T", "no change", "hardDark"],
-                           ["original", "H", "no change", "easyBright"], ["original", "L", "no change", "easyBright"],
-                           ["original", "T", "no change", "easyBright"], ["scrambled", "H", "no change", "easyBright"],
-                           ["scrambled", "L", "no change", "easyBright"], ["scrambled", "T", "no change", "easyBright"],
-                           ["original", "H", "no change", "hardBright"], ["original", "L", "no change", "hardBright"],
-                           ["original", "T", "no change", "hardBright"], ["scrambled", "H", "no change", "hardBright"],
-                           ["scrambled", "L", "no change", "hardBright"], ["scrambled", "T", "no change", "hardBright"]
-                           ]
+#        self.conditions = [["original", "H", "change", "easyDark"], ["original", "L", "change", "easyDark"],
+#                           ["original", "T", "change", "easyDark"], ["scrambled", "H", "change", "easyDark"],
+#                           ["scrambled", "L", "change", "easyDark"], ["scrambled", "T", "change", "easyDark"],
+#                           ["original", "H", "change", "hardDark"], ["original", "L", "change", "hardDark"],
+#                           ["original", "T", "change", "hardDark"], ["scrambled", "H", "change", "hardDark"],
+#                           ["scrambled", "L", "change", "hardDark"], ["scrambled", "T", "change", "hardDark"],
+#                           ["original", "H", "change", "easyBright"], ["original", "L", "change", "easyBright"],
+#                           ["original", "T", "change", "easyBright"], ["scrambled", "H", "change", "easyBright"],
+#                           ["scrambled", "L", "change", "easyBright"], ["scrambled", "T", "change", "easyBright"],
+#                           ["original", "H", "change", "hardBright"], ["original", "L", "change", "hardBright"],
+#                           ["original", "T", "change", "hardBright"], ["scrambled", "H", "change", "hardBright"],
+#                           ["scrambled", "L", "change", "hardBright"], ["scrambled", "T", "change", "hardBright"],
+#                           ["original", "H", "no change", "easyDark"], ["original", "L", "no change", "easyDark"],
+#                           ["original", "T", "no change", "easyDark"], ["scrambled", "H", "no change", "easyDark"],
+#                           ["scrambled", "L", "no change", "easyDark"], ["scrambled", "T", "no change", "easyDark"],
+#                           ["original", "H", "no change", "hardDark"], ["original", "L", "no change", "hardDark"],
+#                           ["original", "T", "no change", "hardDark"], ["scrambled", "H", "no change", "hardDark"],
+#                           ["scrambled", "L", "no change", "hardDark"], ["scrambled", "T", "no change", "hardDark"],
+#                           ["original", "H", "no change", "easyBright"], ["original", "L", "no change", "easyBright"],
+#                           ["original", "T", "no change", "easyBright"], ["scrambled", "H", "no change", "easyBright"],
+#                           ["scrambled", "L", "no change", "easyBright"], ["scrambled", "T", "no change", "easyBright"],
+#                           ["original", "H", "no change", "hardBright"], ["original", "L", "no change", "hardBright"],
+#                           ["original", "T", "no change", "hardBright"], ["scrambled", "H", "no change", "hardBright"],
+#                           ["scrambled", "L", "no change", "hardBright"], ["scrambled", "T", "no change", "hardBright"]
+#                           ]
+        #self.conditions = [["original", "H", "change", "easyBright", "imagery"], ["scrambled", "L", "change", "easyBright","imagery"],
+        #                    ["original", "H", "change", "easyBright", "no imagery"], ["scrambled", "L", "change", "easyBright", "no imagery"],
+        #                    ["original", "H", "no change", "easyBright", "imagery"], ["scrambled", "L", "no change", "easyBright","imagery"],
+        #                    ["original", "H", "no change", "easyBright", "no imagery"], ["scrambled", "L", "no change", "easyBright", "no imagery"]]
+
+        self.conditions = [["scrambled", "+", "change", "easyBright", "imagery"],["original", "+", "change", "easyBright", "imagery"],
+        ["scrambled", "+", "change", "easyBright", "imagery"],["o", "+", "change", "easyBright", "imagery"],["scrambled", "+", "change", "easyBright", "imagery"]]
+
         self.num_trials = len(self.conditions)
         self.num_blocks = 2
 
@@ -103,6 +115,7 @@ class Runtrial:
         self.square_size = 6
         self.border_W = 0.5
         self.grid_range = np.arange(-3, 4, self.square_size / 2)
+        print(self.grid_range)
         self.standard_opacity = 0.30
         fr = 1 / float(60)
         one_s = fr * 60
@@ -117,7 +130,7 @@ class Runtrial:
                                         color=self.font_color, pos=(0, 0))
         self.timer = visual.TextStim(self.win, font=self.font_name, height=self.font_size,
                                      color=self.font_color, pos=(0, 5))
-        self.grid = range(self.grid_size * self.grid_size)
+        self.grid = list(range(self.grid_size * self.grid_size))
         i = 0
         for y in np.arange(-3, 4, self.square_size / 2):
             for x in np.arange(-3, 4, self.square_size / 2):
@@ -135,13 +148,18 @@ class Runtrial:
                         [self.grid_range[2], self.grid_range[2]],
                         [self.grid_range[1], self.grid_range[1]],
                         [self.grid_range[1], self.grid_range[0]]]
-        self.letterH = [[self.grid_range[0], self.grid_range[2]],
-                        [self.grid_range[0], self.grid_range[1]],
-                        [self.grid_range[0], self.grid_range[0]],
-                        [self.grid_range[1], self.grid_range[1]],
-                        [self.grid_range[2], self.grid_range[2]],
+#        self.letterH = [[self.grid_range[0], self.grid_range[2]],
+#                        [self.grid_range[0], self.grid_range[1]],
+#                        [self.grid_range[0], self.grid_range[0]],
+#                        [self.grid_range[1], self.grid_range[1]],
+#                        [self.grid_range[2], self.grid_range[2]],
+#                        [self.grid_range[2], self.grid_range[1]],
+#                        [self.grid_range[2], self.grid_range[0]]]
+        self.plus = [[self.grid_range[0], self.grid_range[1]],
                         [self.grid_range[2], self.grid_range[1]],
-                        [self.grid_range[2], self.grid_range[0]]]
+                        [self.grid_range[1], self.grid_range[2]],
+                        [self.grid_range[1], self.grid_range[1]],
+                        [self.grid_range[1], self.grid_range[0]]]
         self.fullGrid = [[self.grid_range[0], self.grid_range[0]],
                          [self.grid_range[0], self.grid_range[1]],
                          [self.grid_range[0], self.grid_range[2]],
@@ -156,16 +174,26 @@ class Runtrial:
         for i in range(self.grid_size * self.grid_size):
             self.grid[i].draw()
 
+    #for getting the indexes of a shape from fullGrid
+    def getGridIndex(self, shape):
+        indices = []
+        for s in shape:
+            i = self.fullGrid.index(s)
+            indices.append(i)
+        return indices
+
     def __run_trials(self, block_num, num_trials):
 
-        random.shuffle(self.conditions)
+        #random.shuffle(self.conditions)
 
         for trial_num in range(num_trials):
             self.condition = self.conditions[trial_num]
+            print (self.condition)
             which_tiles = self.condition[0]
             which_letter = self.condition[1]
             is_change = self.condition[2]
             which_change = self.condition[3]
+            imagery = self.condition[4]
 
             print (self.condition)
 
@@ -174,20 +202,122 @@ class Runtrial:
             if which_tiles == "original":
                 if which_letter == "L":
                     shade_letter = self.letterL
-                elif which_letter == "H":
-                    shade_letter = self.letterH
+                elif which_letter == "+":
+                    shade_letter = self.plus
                 elif which_letter == "T":
                     shade_letter = self.letterT
             elif which_tiles == "scrambled":
-                shade_letter = random.sample(self.fullGrid, 5)
+                #shade_letter = random.sample(self.fullGrid, 5)
+
+                #until connected object is found
+
+                is_connected = 0 #0 if untested, -1 if failed test, 1 if passed
+                #until solution has been found
+                while is_connected != 1:
+                    #sample an object
+                    shade_letter = random.sample(self.fullGrid, 5)
+                    print("shade letter ", shade_letter)
+                    is_connected = 0
+                    index = 0
+
+                    indices_shade_letter = self.getGridIndex(shade_letter)
+                    indices_L = self.getGridIndex(self.letterL)
+                    indices_T = self.getGridIndex(self.letterT)
+                    indices_plus = self.getGridIndex(self.plus)
+                    #outlaw L-shape
+                    if sorted(indices_shade_letter) == sorted(indices_L):
+                        is_connected = -1 #fail
+                        print("failed L")
+                    #outlaw plus
+                    elif sorted(indices_shade_letter) == sorted(indices_plus):
+                        is_connected = -1 #fail
+                    # #outlaw T
+                    elif sorted(indices_shade_letter) == sorted(indices_T):
+                        is_connected = -1 #fail
+                        print("failed T")
+
+                    #other outlawed shapes, in index
+                    bad_shapes = [[0,1,2,6,7],[0,1,2,7,8],[2,5,8,0,3],[2,5,8,0,3],[8,7,6,1,2],[8,7,6,0,1],[0,3,6,2,5],[0,3,6,5,8],[0,1,3,8,7],[0,1,3,5,8],[2,1,5,7,6],[2,1,5,3,6],[8,5,7,1,0],[8,5,7,0,3],[6,3,7,2,1],[6,3,7,2,5]]
+                    for elem in bad_shapes:
+                        if sorted(indices_shade_letter) == sorted(elem):
+                            is_connected = -1 #fail
+                            print("failed disconnected")
+
+
+                    #while testing isn't done
+                    while is_connected == 0:
+                        #testing that each letter has a neighbor
+                        s = shade_letter[index]
+                        print("s ", s)
+                        i = self.fullGrid.index(s)
+                        print("i ", i)
+
+                        #define potential neighbors
+                        neighbors_i = []
+
+                        if i == 0:
+                            #up
+                            neighbors_i.append(i+1)
+                            #right
+                            neighbors_i.append(i+3)
+                        elif i == 1:
+                            #down
+                            neighbors_i.append(i-1)
+                            neighbors_i.append(i+3)
+                            neighbors_i.append(i+1)
+                        elif i == 2:
+                            #right
+                            neighbors_i.append(i+3)
+                            #down
+                            neighbors_i.append(i-1)
+                        elif i == 3:
+                            neighbors_i.append(i-3)
+                            neighbors_i.append(i+3)
+                            neighbors_i.append(i+1)
+                        elif i == 4:
+                            neighbors_i.append(i-3)
+                            neighbors_i.append(i+3)
+                            neighbors_i.append(i+1)
+                            neighbors_i.append(i-1)
+                        elif i == 5:
+                            neighbors_i.append(i-3)
+                            neighbors_i.append(i+3)
+                            neighbors_i.append(i-1)
+                        elif i == 6:
+                            neighbors_i.append(i-3)
+                            neighbors_i.append(i+1)
+                        elif i == 7:
+                            neighbors_i.append(i-3)
+                            neighbors_i.append(i+1)
+                            neighbors_i.append(i-1)
+                        elif i == 8:
+                            neighbors_i.append(i-3)
+                            neighbors_i.append(i-1)
+
+                        print("neighbors_i", neighbors_i)
+
+                        #if a neighbor isn't included, fail test
+                        is_connected = -1 #fail, unless a neighbor is in shade_letter
+                        for j in neighbors_i:
+                            if self.fullGrid[j] in shade_letter:
+                                is_connected = 0 #testing continues
+                                print("passing test")
+
+                        if index < 4: #five squares means
+                            index = index +1
+                        elif is_connected == 0: #done with testing
+                            is_connected = 1
 
             # Begins the actual trial
             increment = 0.001
-            final_opacity = 0.30
+            final_opacity = 0.50 #Joan had it at 0.30
             self.counter1 = 0  # This counter is for the countdown timing
             self.time = 5
             self.timer.text = self.time
-            self.instruct.text = "Imagine the letter %s." % which_letter
+            if imagery == "imagery":
+                self.instruct.text = "Imagine the symbol %s." % which_letter
+            else:
+                self.instruct.text = "Simply look."
             self.instruct.pos = (0, 6.5)
             self.trial_clock.reset()
             while True:
@@ -204,7 +334,7 @@ class Runtrial:
                 if self.trial_clock.getTime() > 3:
                     if is_change == "change":
                         if which_change == "easyBright":
-                            limit = 0.22
+                            limit = 0.10 #Joan had it at 0.22
                         elif which_change == "hardBright":
                             limit = 0.27
                         elif which_change == "easyDark":
@@ -256,6 +386,7 @@ class Runtrial:
                 trial_data["which_tiles"] = which_tiles
                 trial_data["letter"] = which_letter
                 trial_data["is_change"] = is_change
+                trial_data["imagery"] = imagery
                 trial_data["direction"] = which_change
                 trial_data["final_opacity"] = final_opacity
                 trial_data["response_key"] = response_key
@@ -265,7 +396,7 @@ class Runtrial:
                 os.fsync(self.__csv_file.fileno())
 
     def __run_practice(self):
-        self.instruct.text = """This task is designed to be difficult!  Sometimes, you might have a pretty clear intuition of how many notes played.  Much more often, you won"t be confident at all.  
+        self.instruct.text = """This task is designed to be difficult!  Sometimes, you might have a pretty clear intuition of how many notes played.  Much more often, you won"t be confident at all.
 
 This is okay.  Just let the notes wash over you and give your best guess of the total number of musical notes that played, regardless of how confident you are.
 
@@ -321,7 +452,7 @@ When you are ready, press the spacebar twice to begin.""" % (block_num, self.num
         self.instruct.pos = (0, 0)
         self.instruct.height = self.fontSize
         while True:
-            self.instruct.text = """When you press the spacebar, the screen will exit.  Please wait for a couple moments.  
+            self.instruct.text = """When you press the spacebar, the screen will exit.  Please wait for a couple moments.
 
 Several questions will then appear. Press TAB to move to the next question, and press enter ONLY when you are done with all the questions.
 
